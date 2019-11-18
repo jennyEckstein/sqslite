a22ed74a7b65fa70e3cbeb7673bd0d6c65b6bc29
Eckstein, Jenny (CORP) <Jenny.Eckstein@adp.com>
2019-11-18 13:33:58 -0500

    Added basic CLI

---
+++ bin/cli.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const argv = require('minimist')(process.argv.slice(2));
+const sqsLiteServer = require('../lib');
+
+const DEFAULT_PORT = 4576;
+
+if (argv.help) {
+  return console.log(
+    [
+      ` '',
+  'Usage: sqslite [--port <port>] [--path <path>] [options]',
+  '',
+  'A SQS http server',
+  '',
+  'Options:',
+  '--help                 Display this help message and exit',
+  '--port <port>          The port to listen on (default: 4567)',
+  '',
+  'Report bugs at github.com/jennyEckstein/sqslite/issues'`
+    ].join('\n')
+  );
+}
+
+sqsLiteServer.listen(argv.port || DEFAULT_PORT, (err, address) => {
+  if (err) throw err;
+  sqsLiteServer.log.info(`server listening on ${address}`);
+});
